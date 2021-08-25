3aa99bb5b498c25a71b82840703f327c675ed344
Esranur Türkmen <turkmenesranur@gmail.com>
2021-08-26 00:10:52 +0300

    .

---
+++ ReCapProject/ReCapProject/DataAccess/Concrete/EntityFramework/RentACarContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class RentACarContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-3871KFE;Database=RentACar;Trusted_Connection=true");
+        }
+
+        public DbSet<Car> Car { get; set; }
+
+        public DbSet<Brand> Brand { get; set; }
+
+        public DbSet<Color> Color { get; set; }
+
+
+    }
+}
---
+++ ReCapProject/ReCapProject/Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //bu interface i implement eden her sınıf bir veritabanı tablosudur.
+    public interface IEntity
+
+    {
+    }
+}
